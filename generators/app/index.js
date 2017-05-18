5ab7a42792ec803785b6dd2aa2ffbf3d55207514
Miguel Palau <mpalauzarza@gmail.com>
2017-05-18 18:01:25 -0500

    test commands

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,1 +1,9 @@
-module.exports = console.log('Shopify Skeleton')
+const Generator = require('yeoman-generator')
+
+class ShopifySkeleton extends Generator {
+  sayHello () {
+    console.log('Hello from Pixel2HTML')
+  }
+}
+
+module.exports = ShopifySkeleton
