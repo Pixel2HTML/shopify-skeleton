adc6ab50c6a22ce7d896487af0979a6527a8da1b
Miguel Palau <mpalauzarza@gmail.com>
2017-07-17 17:50:06 -0500

    adding starting styles task

---
+++ src/app/templates/gulp/gulp.config.js
@@ -0,0 +1,30 @@
+'use strict'
+// We use this to read flags in the command line
+const argv = require('yargs').argv
+// Add your conditions here 💅
+const production = argv.prod || argv.production
+
+module.exports = {
+  src: {
+    scssMain: [
+      './src/styles/main.scss'
+    ],
+    scssFonts: [
+      './src/styles/fonts.scss'
+    ],
+    scssVendors: [
+      './src/styles/vendor.scss'
+    ]
+  },
+  dist: {
+    assets: './deploy/assets'
+  },
+  scriptVendors: [
+    'jquery'
+  ],
+  onError: function (error) {
+    console.log(error.toString())
+    this.emit('end')
+  },
+  production: !!production
+}
