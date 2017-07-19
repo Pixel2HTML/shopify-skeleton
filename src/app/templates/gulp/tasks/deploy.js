7643286ad3df2cd0a32c603832559089fcf1d0c6
Miguel Palau <mpalauzarza@gmail.com>
2017-07-19 16:36:13 -0500

    more conditional deploy

--- src/app/templates/gulp/tasks/deploy.js
+++ src/app/templates/gulp/tasks/deploy.js
@@ -3,8 +3,11 @@ const watch = require('gulp-watch')
 const config = require('../gulp.config')
 const shopify = require('gulp-shopify-upload-with-callbacks')
 
+const WE_CAN_DEPLOY = config.shopify.key && config.shopify.pass && config.shopify.shopName && config.shopify.themeId
+
 gulp.task('deploy', () => {
-  return watch(config.theme + '/{assets|layout|config|snippets|templates|locales|sections}/**/*')
+  return WE_CAN_DEPLOY
+    ? watch(config.theme + '/{assets|layout|config|snippets|templates|locales|sections}/**/*')
     .pipe(shopify(
       config.shopify.key,
       config.shopify.pass,
@@ -12,4 +15,5 @@ gulp.task('deploy', () => {
       config.shopify.themeId,
       {basePath: config.theme}
     ))
+    : true
 })
