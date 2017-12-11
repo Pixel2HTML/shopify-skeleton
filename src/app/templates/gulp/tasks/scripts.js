6ddc04a4680479a2029379c01632f4c4af6a3bed
Miguel Palau <mpalauzarza@gmail.com>
2017-12-11 16:52:50 -0600

    proper scripts task

--- src/app/templates/gulp/tasks/scripts.js
+++ src/app/templates/gulp/tasks/scripts.js
@@ -3,19 +3,9 @@ const config = require('../gulp.config')
 const webpack = require('webpack')
 const wp = require('webpack-stream')
 const webpackConfig = require('../../webpack.config')
-const $ = require('gulp-load-plugins')()
 
-gulp.task('webpack', () =>
+gulp.task('scripts', () =>
   gulp.src(config.src.scripts + '/index.js')
     .pipe(wp(webpackConfig, webpack)).on('error', config.onError)
     .pipe(gulp.dest(config.theme + '/assets'))
 )
-
-gulp.task('legacy', () =>
-  gulp.src(config.src.scripts + '/__legacy__/*.js')
-    .pipe($.concat('js_libs.js'))
-    .pipe($.uglify())
-    .pipe(gulp.dest(config.theme + '/assets'))
-)
-
-gulp.task('scripts', gulp.parallel('webpack', 'legacy'))
