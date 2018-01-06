384d15206e17fcc526566f46644fc9a032340efa
Miguel Palau <mpalauzarza@gmail.com>
2018-01-05 20:36:02 -0600

    remove webpack dependencies

--- src/app/templates/gulp/tasks/scripts.js
+++ src/app/templates/gulp/tasks/scripts.js
@@ -1,11 +1,11 @@
 const gulp = require('gulp')
-const config = require('../gulp.config')
 const webpack = require('webpack')
-const wp = require('webpack-stream')
 const webpackConfig = require('../../webpack.config')
 
 gulp.task('scripts', () =>
-  gulp.src(config.src.scripts + '/index.js')
-    .pipe(wp(webpackConfig, webpack)).on('error', config.onError)
-    .pipe(gulp.dest(config.theme + '/assets'))
+  new Promise(resolve => webpack(webpackConfig, (err, stats) => {
+    if (err) console.log('Webpack', err)
+    console.log(stats.toString())
+    resolve()
+  }))
 )
